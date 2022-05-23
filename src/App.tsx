dea13b9d025400c58330b68e72f1e798bc93f373
Vladimir <vladimir.yakimov.9811@gmail.com>
2022-05-23 17:15:15 +0300

    after 9 lessons

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,13 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
+import Technologies from './Technologies';
 
 const App = () => {
     return (
         <div>
-            <div>one div</div>
-            <div className="App">
-                <ul>
-                    <li>css</li>
-                    <li>html</li>
-                    <li>js</li>
-                    <li>react</li>
-                </ul>
-            </div>
+            <Header />
+            <Technologies />
         </div>
     );
 }
