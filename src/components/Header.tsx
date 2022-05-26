0a076f5f15bbcf1a55438177de416f476a97d298
Vladimir <vladimir.yakimov.9811@gmail.com>
2022-05-27 00:34:50 +0300

    After 13 lessons

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img
+                src="https://avatars.mds.yandex.net/i?id=b462fe80092ada87ba33307cdf12bf62-4472919-images-thumbs&n=13"
+                alt="logo"/>
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src="https://ic.pics.livejournal.com/stepbystep_hdr/11824494/1199598/1199598_original.jpg"
+                     alt="обложка"/>
+            </div>
+            <div>
+                ava + desription
+            </div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
